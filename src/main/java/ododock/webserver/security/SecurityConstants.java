254fb50ff27ed031d49d4718035d773313eade58
Joonwoo Park <93jpark@gmail.com>
2024-03-17 16:27:45 +0900

    fix: adjust token expiration

--- src/main/java/ododock/webserver/security/SecurityConstants.java
+++ src/main/java/ododock/webserver/security/SecurityConstants.java
@@ -1,8 +1,8 @@
 package ododock.webserver.security;
 
 public final class SecurityConstants {
-    public static final long JWT_ACCESS_EXPIRATION = 70000;
-    public static final long JWT_REFRESH_EXPIRATION = 140000;
+    public static final long JWT_ACCESS_EXPIRATION = 600000L;
+    public static final long JWT_REFRESH_EXPIRATION = 86400000L;
 
     private SecurityConstants() {
         throw new UnsupportedOperationException();
