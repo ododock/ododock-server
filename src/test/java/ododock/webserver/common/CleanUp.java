0a8c1624e49281f02e742c9a586a142d48c1f3bf
Joonwoo Park <93jpark@gmail.com>
2024-03-24 17:34:47 +0900

    test: fix way to clean up

--- src/test/java/ododock/webserver/common/CleanUp.java
+++ src/test/java/ododock/webserver/common/CleanUp.java
@@ -6,6 +6,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class CleanUp {
 
@@ -20,9 +23,12 @@ public class CleanUp {
 
     @Transactional
     public void all() {
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+        List<String> tables = new ArrayList<>();
         em.getMetamodel().getEntities().forEach(entityType -> {
-            final String tableName = entityType.getJavaType().getAnnotation(Table.class).getClass().getName();
+            final String tableName = entityType.getJavaType().getAnnotation(Table.class).name();
             jdbcTemplate.execute("TRUNCATE TABLE " + tableName);
         });
+        jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
     }
 }
