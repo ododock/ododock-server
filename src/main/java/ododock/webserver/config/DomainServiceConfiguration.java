bbe52155d81c2bb455e57897403d3122df58d4ef
Joonwoo Park <93jpark@gmail.com>
2024-06-09 21:18:22 +0900

    feature: refresh token authentication (#10)
    * config: ignore static resources
    
    * feature: add date time serializer dependency
    
    * chore: maintenance
    
    * feature: add refresh token authentication filter

--- src/main/java/ododock/webserver/config/DomainServiceConfiguration.java
+++ src/main/java/ododock/webserver/config/DomainServiceConfiguration.java
@@ -1,8 +1,5 @@
 package ododock.webserver.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,14 +11,6 @@ public class DomainServiceConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.registerModule(new Jdk8Module());
-        return objectMapper;
     }
 
 }
