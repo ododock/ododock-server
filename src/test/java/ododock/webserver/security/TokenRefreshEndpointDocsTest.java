2a53d15e4b5bbb2d429ba5521eeed1ba822e1028
Joonwoo Park <93jpark@gmail.com>
2024-06-27 11:04:48 +0900

    docs: token refresh endpoint

--- src/test/java/ododock/webserver/security/TokenRefreshEndpointDocsTest.java
+++ src/test/java/ododock/webserver/security/TokenRefreshEndpointDocsTest.java
@@ -8,6 +8,7 @@ import ododock.webserver.domain.account.TokenRecord;
 import ododock.webserver.repository.AccountRepository;
 import ododock.webserver.request.account.AccountCreate;
 import ododock.webserver.security.filter.RefreshTokenAuthenticationFilter;
+import ododock.webserver.security.response.DaoUserDetails;
 import ododock.webserver.security.response.UserPrincipal;
 import ododock.webserver.security.service.JwtService;
 import ododock.webserver.service.AccountService;
@@ -19,31 +20,26 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -53,6 +49,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 public class TokenRefreshEndpointDocsTest {
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Autowired
     private MockMvc mockMvc;
@@ -72,18 +71,13 @@ public class TokenRefreshEndpointDocsTest {
     @Autowired
     private JwtDecoder jwtDecoder;
 
-    @Autowired
-    private JwtEncoder jwtEncoder;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @BeforeEach
-    public void setup() {
+    public void setup(RestDocumentationContextProvider restDocumentation) {
         RefreshTokenAuthenticationFilter filter = new RefreshTokenAuthenticationFilter(jwtDecoder, jwtService, objectMapper);
-        this.mockMvc = MockMvcBuilders
-                .standaloneSetup(new Object()) // 더미 컨트롤러
-                .addFilter(filter)
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(print())
+                .addFilters(filter)
                 .build();
     }
 
@@ -112,39 +106,18 @@ public class TokenRefreshEndpointDocsTest {
         TokenRecord tokenRecord = jwtService.generateToken(UserPrincipal.from((DaoUserDetails) authentication.getPrincipal()));
         String refreshToken = tokenRecord.getRefreshTokenValue();
 
-
         // When & Then
-        mockMvc.perform(post("/api/v1/login/refresh")
+        mockMvc.perform(post("/api/v1/auth/token")
                         .cookie(new Cookie("refresh_token", refreshToken)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").value("new_access_token"))
                 .andDo(print())
                 .andDo(document("refresh-token",
                         responseFields(
-                                fieldWithPath("token").description("새로 발급된 액세스 토큰")
-                        )
-                ));
-
-    }
-
-    @Test
-    public void testRefreshTokenAuthentication_InvalidToken() throws Exception {
-        // Given
-        String invalidRefreshToken = "invalid_refresh_token";
-
-        given(jwtDecoder.decode(anyString())).willThrow(new Exception("Invalid token"));
-
-        // When & Then
-        mockMvc.perform(post("/api/v1/login/refresh")
-                        .cookie(new Cookie("refresh_token", invalidRefreshToken)))
-                .andExpect(status().isUnauthorized())
-                .andDo(print())
-                .andDo(document("refresh-token-invalid",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint()),
-                        responseFields(
-                                fieldWithPath("error").description("에러 메시지")
+                                fieldWithPath("sub").description("유저 ID"),
+                                fieldWithPath("accessToken").description("새로 발급된 액세스 토큰"),
+                                fieldWithPath("refreshToken").description("새로 발급된 리프레시 토큰")
                         )
                 ));
     }
+
 }
