51dbdfa28a94c24c46038a73cd76a36d516f785b
Joonwoo Park <93jpark@gmail.com>
2024-07-27 17:20:41 +0900

    chore: add field for ListResponse

--- src/main/java/ododock/webserver/response/ListResponse.java
+++ src/main/java/ododock/webserver/response/ListResponse.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 public record ListResponse<T>(
         @Nullable Long ownerId,
+        @Nullable Integer totalSize,
         List<T> content
 ) {
 
-    public static <T> ListResponse<T> of(@Nullable final Long ownerId, final List<T> content) {
-        return new ListResponse<>(ownerId, content);
+    public static <T> ListResponse<T> of(@Nullable final Long ownerId, @Nullable final Integer totalSize, final List<T> content) {
+        return new ListResponse<>(ownerId, totalSize, content);
     }
 }
