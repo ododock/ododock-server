8d2e47ed0073e2564d0b0da44ba4901ddc57fcac
Joonwoo Park <93jpark@gmail.com>
2024-07-28 18:40:36 +0900

    fix: fix token validation (#23)
    * fix: fix token validation
    
    * update github action workflow

--- src/main/java/ododock/webserver/security/config/OAuth2SecurityConfig.java
+++ src/main/java/ododock/webserver/security/config/OAuth2SecurityConfig.java
@@ -6,6 +6,7 @@ import ododock.webserver.security.service.AuthService;
 import ododock.webserver.security.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,24 +17,18 @@ public class OAuth2SecurityConfig {
 
     private final AuthService authService;
     private final JwtService jwtService;
-    private final JwtDecoder jwtDecoder;
 
     @Bean
+    @Order
     public SecurityFilterChain oauth2Chain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(authorizeRequest -> authorizeRequest
                         .anyRequest()
-                        .authenticated()
+                        .denyAll()
                 )
                 .oauth2Login(login -> login.userInfoEndpoint(
-                        info -> info.userService(authService)
-                ).successHandler(oAuth2LoginSuccessHandler(jwtService))
-                )
-
-                .oauth2ResourceServer(
-                        resourceServer -> resourceServer.jwt(
-                                jwt -> jwt.decoder(jwtDecoder)
-                        )
+                                info -> info.userService(authService)
+                        ).successHandler(oAuth2LoginSuccessHandler(jwtService))
                 )
                 .build();
     }
