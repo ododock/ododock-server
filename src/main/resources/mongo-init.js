cfc7fa1c8c6a64ca4d9814eed2343e46904e9ee7
Joonwoo Park <93jpark@gmail.com>
2025-03-22 23:29:45 +0900

    chore: disable embedded mongodb

--- src/main/resources/mongo-init.js
+++
@@ -1,8 +0,0 @@
-// mongo-init.js
-print("Creating 'oddk' database...");
-
-db = db.getSiblingDB('oddk');
-db.createCollection('test_collection');
-db.test_collection.insertOne({ message: "Database initialized!" });
-
-print("Database 'oddk' initialized successfully.");
