a4f83ed43f58987f0b45de6325044c19a5fe374c
Joonwoo Park <93jpark@gmail.com>
2025-04-13 23:07:28 +0900

    docs: add profile controller docs test

--- src/test/java/ododock/webserver/controller/V1alpha1ProfileControllerDocsTest.java
+++ src/test/java/ododock/webserver/controller/V1alpha1ProfileControllerDocsTest.java
@@ -4,13 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ododock.webserver.common.RestDocsConfig;
 import ododock.webserver.common.TestMvcSecurityConfig;
 import ododock.webserver.config.web.WebConfiguration;
-import ododock.webserver.domain.account.Account;
-import ododock.webserver.domain.account.ProfileImage;
-import ododock.webserver.domain.account.S3ProfileService;
+import ododock.webserver.domain.profile.Profile;
+import ododock.webserver.domain.profile.ProfileImage;
+import ododock.webserver.domain.profile.ProfileService;
 import ododock.webserver.web.ResourcePath;
 import ododock.webserver.web.v1alpha1.V1alpha1ProfileController;
+import ododock.webserver.web.v1alpha1.dto.account.ImageFile;
 import ododock.webserver.web.v1alpha1.dto.account.V1alpha1Account;
 import ododock.webserver.web.v1alpha1.dto.account.V1alpha1Profile;
+import ododock.webserver.web.v1alpha1.dto.account.V1alpha1ProfileImage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -18,20 +20,21 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.resourceDetails;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
+import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,23 +52,22 @@ public class V1alpha1ProfileControllerDocsTest {
     private ObjectMapper objectMapper;
 
     @MockBean
-    private S3ProfileService s3ProfileService;
+    private ProfileService profileService;
 
     @Test
     @WithMockUser
     void getProfile_Docs() throws Exception {
         // given
-        final Account response = Account.builder()
-                .id(1L)
-                .nickname("user1")
-                .fullname("John Doe")
-                .birthDate(LocalDate.of(1990, 1, 1))
-                .profileImage(ProfileImage.builder()
-                        .imageSource("http://oddx.xyz/img.jpg")
-                        .fileType("jpeg")
-                        .build())
-                .build();
-        given(s3ProfileService.getProfile(1L)).willReturn(response);
+        Profile profile = mock(Profile.class);
+        given(profile.getNickname()).willReturn("user1");
+        given(profile.getFullname()).willReturn("John Doe");
+        given(profile.getBirthDate()).willReturn(LocalDate.of(1990, 1, 1));
+        given(profile.getProfileImage()).willReturn(ododock.webserver.domain.profile.ProfileImage.builder()
+                .sourcePath("/img.jpg")
+                .fileType("jpeg")
+                .build());
+
+        given(profileService.getProfile(1L)).willReturn(profile);
 
         // expected
         mockMvc.perform(
@@ -82,12 +84,11 @@ public class V1alpha1ProfileControllerDocsTest {
                                         parameterWithName("id").description("조회할 프로필 ID")
                                 ),
                                 responseFields(
-                                        fieldWithPath("id").description("조회한 프로필의 소유자 Account ID"),
                                         fieldWithPath("nickname").description("조회한 프로필의 닉네임"),
                                         fieldWithPath("fullname").description("조회한 프로필의 닉네임"),
                                         fieldWithPath("birthDate").description("조회한 프로필의 생년월일"),
-                                        fieldWithPath("profileImageSource").description("조회한 프로필의 프로필 사진 리소스 주소"),
-                                        fieldWithPath("profileImageFileType").description("조회한 프로필의 프로필 사진 리소스 타입")
+                                        fieldWithPath("profileImage.sourcePath").description("조회한 프로필의 프로필 사진 리소스 주소"),
+                                        fieldWithPath("profileImage.fileType").description("조회한 프로필의 프로필 사진 리소스 타입")
                                 )
                         )
                 );
@@ -99,8 +100,10 @@ public class V1alpha1ProfileControllerDocsTest {
         // given
         final V1alpha1Profile request = V1alpha1Profile.builder()
                 .nickname("newNickname")
-                .profileImageSource("http://oddk.xyz/newPhoto.png")
-                .profileImageFileType("png")
+                .profileImage(V1alpha1ProfileImage.builder()
+                        .sourcePath("newPhoto.png")
+                        .fileType("png")
+                        .build())
                 .build();
 
         final V1alpha1Account response = V1alpha1Account.builder()
@@ -128,8 +131,92 @@ public class V1alpha1ProfileControllerDocsTest {
                                 ),
                                 requestFields(
                                         fieldWithPath("nickname").description("업데이트할 프로필의 닉네임"),
-                                        fieldWithPath("profileImageSource").description("업데이트할 프로필의 프로필 사진 리소스 주소"),
-                                        fieldWithPath("profileImageFileType").description("업데이트할 프로필의 프로필 사진 파일 포맷")
+                                        fieldWithPath("profileImage.sourcePath").description("업데이트할 프로필의 프로필 사진 리소스 주소"),
+                                        fieldWithPath("profileImage.fileType").description("업데이트할 프로필의 프로필 사진 파일 포맷")
+                                )
+                        )
+                );
+    }
+
+    @Test
+    @WithMockUser
+    void getProfileImage_Docs() throws Exception {
+        // given
+        ProfileImage profileImage = mock(ProfileImage.class);
+        given(profileImage.getSourcePath()).willReturn("/img.jpg");
+        given(profileImage.getFileType()).willReturn("jpeg");
+
+        given(profileService.getProfileImage(1L)).willReturn(Optional.of(profileImage));
+
+        // expected
+        mockMvc.perform(
+                        get(BASE_PATH + "/{" + ResourcePath.PATH_VAR_ID + "}" + ResourcePath.ACCOUNTS_SUBRESOURCE_PROFILE + ResourcePath.PROFILE_SUBRESOURCE_IMAGE, 1L)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .characterEncoding(StandardCharsets.UTF_8))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("profile/get-profile-image",
+                                resourceDetails()
+                                        .tag("Profile").description("계정 프로필 이미지 조회 엔드포인트"),
+                                pathParameters(
+                                        parameterWithName("id").description("조회할 프로필 ID")
+                                ),
+                                responseFields(
+                                        fieldWithPath("sourcePath").description("조회한 프로필의 프로필 사진 리소스 주소"),
+                                        fieldWithPath("fileType").description("조회한 프로필의 프로필 사진 리소스 타입")
+                                )
+                        )
+                );
+    }
+
+    @Test
+    @WithMockUser
+    void saveProfile_Docs() throws Exception {
+        // given
+        final V1alpha1Profile request = V1alpha1Profile.builder()
+                .nickname("newNickname")
+                .profileImage(V1alpha1ProfileImage.builder()
+                        .sourcePath("newPhoto.png")
+                        .fileType("png")
+                        .build())
+                .build();
+
+        final V1alpha1Account response = V1alpha1Account.builder()
+                .id(1L)
+                .nickname("user1")
+                .fullname("John Doe")
+                .profileImageSource("http://oddx.xyz/img.jpg")
+                .profileImageFileType("jpeg")
+                .build();
+
+        MockMultipartFile multipartFile = new MockMultipartFile("file", "test.png",
+                "image/png", "Spring Framework".getBytes());
+
+        ProfileImage profileImage = mock(ProfileImage.class);
+        given(profileImage.getSourcePath()).willReturn("/img.jpg");
+        given(profileImage.getFileType()).willReturn("jpeg");
+
+        given(profileService.saveProfileImage(1L, ImageFile.from(multipartFile))).willReturn(profileImage);
+
+        // expected
+        mockMvc.perform(
+                        multipart(BASE_PATH + "/{" + ResourcePath.PATH_VAR_ID + "}" + ResourcePath.ACCOUNTS_SUBRESOURCE_PROFILE + ResourcePath.PROFILE_SUBRESOURCE_IMAGE, 1L)
+                                .file(multipartFile)
+                                .contentType(MediaType.MULTIPART_FORM_DATA)
+                                .characterEncoding(StandardCharsets.UTF_8)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(
+                        document("profile/save-profile-image",
+                                resourceDetails()
+                                        .tag("Profile").description("계정 프로필 이미지 저장 엔드포인트"),
+                                pathParameters(
+                                        parameterWithName("id").description("저장할 프로필 이미지의 Account ID")
+                                ),
+                                requestParts(
+                                        partWithName("file").description("업로드 할 프로필 이미지 파일")
                                 )
                         )
                 );
