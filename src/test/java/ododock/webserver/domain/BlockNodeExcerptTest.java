8555c089e8ccaac4a8d62f59ddc004d1fbd2bf17
Joonwoo Park <93jpark@gmail.com>
2025-06-01 15:37:03 +0900

    chore: add excerptor test code

---
+++ src/test/java/ododock/webserver/domain/BlockNodeExcerptTest.java
@@ -0,0 +1,177 @@
+package ododock.webserver.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.reflect.TypeToken;
+import ododock.webserver.domain.article.ArticleExcerptor;
+import ododock.webserver.domain.article.dto.V1alpha1BaseBlock;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BlockNodeExcerptTest {
+
+    @Test
+    public void testExcerpt() {
+        String jsonString = """
+                [
+                    {
+                        "id": "9ab769ea-cfdb-414d-a190-5e9d1dc29d85",
+                        "type": "heading",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left",
+                            "level": 1
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "This is sample article heading",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "3f18f50d-fa94-4d37-b8ad-1661d7249f33",
+                        "type": "heading",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left",
+                            "level": 2
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "Table of contents",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "3b4f8d56-cf4e-4c59-b14e-1a90949154a0",
+                        "type": "numberedListItem",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "abstract",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "09b8e157-14c8-4d74-a408-7363cdbc467d",
+                        "type": "numberedListItem",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "body",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "6e1c46b8-11ba-4aa1-bb33-2d12e9b65cd2",
+                        "type": "numberedListItem",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "summary",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "2a870bb9-4220-40da-965e-1c37acd8d2a1",
+                        "type": "paragraph",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [],
+                        "children": []
+                        },
+                        {
+                        "id": "bc5e0274-3942-4541-8ccb-adaa81e1011f",
+                        "type": "heading",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left",
+                            "level": 3
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "abstract",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "944c73d9-e4d6-4f8d-a178-f8cebcf52ec2",
+                        "type": "paragraph",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [
+                            {
+                            "type": "text",
+                            "text": "Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis.",
+                            "styles": {}
+                            }
+                        ],
+                        "children": []
+                        },
+                        {
+                        "id": "d49597c1-8028-4aac-9b60-5b4f8c46d62b",
+                        "type": "paragraph",
+                        "props": {
+                            "textColor": "default",
+                            "backgroundColor": "default",
+                            "textAlignment": "left"
+                        },
+                        "content": [],
+                        "children": []
+                        }
+                    ]
+                """;
+
+        Type listType = new TypeToken<List<V1alpha1BaseBlock>>() {}.getType();
+        List<V1alpha1BaseBlock> blocks = new Gson().fromJson(jsonString, listType);
+        String excerpt = ArticleExcerptor.from(blocks);
+
+        assertThat(excerpt).isNotBlank();
+        assertThat(excerpt).startsWith("This is sample article heading Table of contents");
+    }
+
+
+}
--- src/test/java/ododock/webserver/domain/BlockNodeJsonTest.java
+++ src/test/java/ododock/webserver/domain/BlockNodeJsonTest.java
@@ -2,10 +2,14 @@ package ododock.webserver.domain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.reflect.TypeToken;
 import ododock.webserver.domain.article.dto.V1alpha1BaseBlock;
 import ododock.webserver.domain.article.dto.V1alpha1DefaultProps;
 import ododock.webserver.domain.article.dto.V1alpha1InlineContent;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,7 +52,6 @@ public class BlockNodeJsonTest {
         V1alpha1InlineContent inline = block.getContent().getFirst();
         assertThat(inline.getType()).isEqualTo("text");
         assertThat(inline.getText()).isEqualTo("Welcome to this demo!");
-
     }
 
 
@@ -57,7 +60,7 @@ public class BlockNodeJsonTest {
         String jsonString = """
                 [
                   {
-                    "id": "22ab8597-47c7-40df-88dc-16a308a2b826",
+                    "id": "9ab769ea-cfdb-414d-a190-5e9d1dc29d85",
                     "type": "heading",
                     "props": {
                       "textColor": "default",
@@ -68,14 +71,32 @@ public class BlockNodeJsonTest {
                     "content": [
                       {
                         "type": "text",
-                        "text": "This is heading",
+                        "text": "This is sample article heading",
                         "styles": {}
                       }
                     ],
                     "children": []
                   },
                   {
-                    "id": "b48f141e-4704-4b73-acf0-ca54c465d84c",
+                    "id": "d957fb97-db32-41c7-bf05-449b1838bb5d",
+                    "type": "heading",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left",
+                      "level": 2
+                    },
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "Table of contents",
+                        "styles": {}
+                      }
+                    ],
+                    "children": []
+                  },
+                  {
+                    "id": "a469bf7c-ff3e-47c4-b612-cedff90d7dec",
                     "type": "numberedListItem",
                     "props": {
                       "textColor": "default",
@@ -85,49 +106,105 @@ public class BlockNodeJsonTest {
                     "content": [
                       {
                         "type": "text",
-                        "text": "numberedList",
+                        "text": "abstract",
                         "styles": {}
                       }
                     ],
-                    "children": [
-                      {
-                        "id": "51f3db79-4f54-4964-b05e-acb2cfcb21dd",
-                        "type": "numberedListItem",
-                        "props": {
-                          "textColor": "default",
-                          "backgroundColor": "default",
-                          "textAlignment": "left"
-                        },
-                        "content": [
-                          {
-                            "type": "text",
-                            "text": "Nested Numbered List1",
-                            "styles": {}
-                          }
-                        ],
-                        "children": []
-                      },
-                      {
-                        "id": "779d4f1b-6e67-4ee3-ac3f-a89631d1b11b",
-                        "type": "numberedListItem",
-                        "props": {
-                          "textColor": "default",
-                          "backgroundColor": "default",
-                          "textAlignment": "left"
-                        },
-                        "content": [
-                          {
-                            "type": "text",
-                            "text": "Nested Numbered List2",
-                            "styles": {}
-                          }
-                        ],
-                        "children": []
-                      }
-                    ]
+                    "children": []
                   },
                   {
-                    "id": "bee14318-604f-4d8c-b715-db77540afe2c",
+                    "id": "5b468a2d-23ff-4764-93c7-4f0ef0bae78b",
+                    "type": "numberedListItem",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left"
+                    },
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "body",
+                        "styles": {}
+                      }
+                    ],
+                    "children": []
+                  },
+                  {
+                    "id": "eba23025-e696-4d61-b668-252a05bf2b2a",
+                    "type": "numberedListItem",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left"
+                    },
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "summary",
+                        "styles": {}
+                      }
+                    ],
+                    "children": []
+                  },
+                  {
+                    "id": "b072c8cb-70fe-4b37-a239-5a3d5f90999e",
+                    "type": "paragraph",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left"
+                    },
+                    "content": [],
+                    "children": []
+                  },
+                  {
+                    "id": "0be83fae-1a57-47b0-b80e-984c058ab0bd",
+                    "type": "heading",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left",
+                      "level": 3
+                    },
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "abstract",
+                        "styles": {}
+                      }
+                    ],
+                    "children": []
+                  },
+                  {
+                    "id": "e243ac50-8eb1-47ba-9dab-1bed1bcbc025",
+                    "type": "paragraph",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left"
+                    },
+                    "content": [
+                      {
+                        "type": "text",
+                        "text": "Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis.",
+                        "styles": {}
+                      }
+                    ],
+                    "children": []
+                  },
+                  {
+                    "id": "29b56e7f-f334-4eab-aac8-30b271c5f073",
+                    "type": "paragraph",
+                    "props": {
+                      "textColor": "default",
+                      "backgroundColor": "default",
+                      "textAlignment": "left"
+                    },
+                    "content": [],
+                    "children": []
+                  },
+                  {
+                    "id": "97b538d3-2a6c-41cd-995a-dd395739cf95",
                     "type": "paragraph",
                     "props": {
                       "textColor": "default",
@@ -139,8 +216,10 @@ public class BlockNodeJsonTest {
                   }
                 ]
                 """;
-        ObjectMapper mapper = new ObjectMapper();
-
+        Type listType = new TypeToken<List<V1alpha1BaseBlock>>() {}.getType();
+        List<V1alpha1BaseBlock> blocks = new Gson().fromJson(jsonString, listType);
+        assertThat(blocks).isNotEmpty();
+        assertThat(blocks.getFirst().getType()).isEqualTo("heading");
     }
 
 }
